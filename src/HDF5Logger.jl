5417c9f1ac2c4a6e562741833668a06e29b3fe16
Tucker McClure <tucker@tuckermcclure.com>
2017-07-19 13:50:07 -0700

    Created basic logger.

--- src/HDF5Logger.jl
+++ src/HDF5Logger.jl
@@ -1,5 +1,80 @@
+"""
+    HDF5Logger
+
+Creates an object for logging frames of data to an HDF5 file. The frames are
+expected to have the same size, and the total number of frames to log is
+expected to be known in advance. It currently works with scalars, vectors, and
+matrices of basic types that the HDF5 package supports.
+
+# Examples
+```julia-repl
+    log = Log("my_file.h5") # Create a logger.
+    num_frames = 100        # Set the total number of frames to log.
+    example_data = [1., 2., 3.] # Create an example of a frame of data.
+    add!(log, "/a_vector", example_data, num_frames) # Create the stream.
+    log!(log, "/a_vector", [4., 5., 6.]) # Log a single frame of data.
+    close!(log) # Always clean up when done. Use try-catch to make sure.
+```
+
+"""
 module HDF5Logger
 
-# package code goes here
+using HDF5
+
+export Log, add!, log!, close!
+
+mutable struct Stream
+    count::Int64
+    length::Int64
+    rank::Int64
+    dataset::HDF5Dataset
+end
+
+struct Log
+    streams::Dict{String,Stream}
+    file_id::HDF5File
+    function Log(file_name::String)
+        new(Dict{String,Stream}(), h5open(file_name, "w"));
+    end
+end
+
+function prepare_group!(log::Log, slug::String)
+    groups = filter(x->!isempty(x), split(slug)) # Explode to group names
+    group_id = g_open(log.file_id, "/") # Start at top group
+    for k = 1:length(groups)-1 # For each group up to dataset
+        if exists(group_id, groups[k])
+            println("Group '$(groups[k]))' already exists.")
+            group_id = g_open(group_id, String(groups[k]))
+        else
+            println("Creating group '$(groups[k]))'.")
+            group_id = g_create(group_id, String(groups[k]))
+        end
+    end
+    return (group_id, String(groups[end])) # Group ID and name of dataset
+end
+
+function add!(log::Log, group_name::String, data, num_samples::Int64)
+    dims = isbits(data) ? 1 : size(data)
+    group_id, group_name    = prepare_group!(log, group_name);
+    dataset_id              = d_create(group_id, group_name,
+                                       datatype(eltype(data)),
+                                       dataspace(dims..., num_samples));
+    log.streams[group_name] = Stream(0, num_samples, length(dims), dataset_id)
+end
+
+function log!(log::Log, slug::String, data)
+    @assert(haskey(log.streams, slug),
+            "The logger doesn't have that key. Perhaps you need to `add` it?")
+    @assert(log.streams[slug].count < log.streams[slug].length,
+            "We've already used up all of the allocated space for this stream!")
+    log.streams[slug].count += 1;
+    index = (repeat([:], outer=log.streams[slug].rank)...,
+             log.streams[slug].count); # TODO: Is there a better way to do this?
+    log.streams[slug].dataset[index...] = data
+end
+
+function close!(log::Log)
+    close(log.file_id)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,67 @@
 using HDF5Logger
 using Base.Test
+using HDF5 # For reading in wholesale time histories
 
-# write your own tests here
-@test 1 == 2
+# Set up some data to work with.
+num_samples = 5
+vector_data = [1., 2., 3.]
+matrix_data = [1 2 3; 4 5 6].'
+scalar_data = 1.
+c           = 2
+
+# Create the log.
+file_name = "test_log.h5"
+log = Log(file_name)
+try
+
+    # Test a vector
+    add!(log, "/vector", vector_data, num_samples)
+    log!(log, "/vector", vector_data)
+    log!(log, "/vector", c * vector_data)
+
+    # Test a matrix
+    add!(log, "/matrix", matrix_data, num_samples)
+    log!(log, "/matrix", matrix_data)
+    log!(log, "/matrix", c * matrix_data)
+
+    # Test a scalar
+    add!(log, "/scalar", scalar_data, num_samples)
+    log!(log, "/scalar", scalar_data)
+    log!(log, "/scalar", c * scalar_data)
+
+catch err
+    close!(log)
+    rethrow(err)
+end
+
+close!(log)
+
+# Now test that the right stuff happened. Use the regular HDF5 library to open
+# the files we created. With the `do` block, the file will be automatically
+# closed if anything goes wrong.
+h5open(file_name, "r") do file
+
+    vector_result = read(file, "/vector")
+    matrix_result = read(file, "/matrix")
+    scalar_result = read(file, "/scalar")
+
+    @test(vector_result[:,1]   == vector_data)
+    @test(matrix_result[:,:,1] == matrix_data)
+    @test(scalar_result[1]     == scalar_data)
+
+    @test(vector_result[:,2]   == c * vector_data)
+    @test(matrix_result[:,:,2] == c * matrix_data)
+    @test(scalar_result[2]     == c * scalar_data)
+
+    @test(size(vector_result) == (3, num_samples))
+    @test(size(matrix_result) == (3, 2, num_samples))
+    @test(size(scalar_result) == (1, num_samples))
+
+    @test(eltype(vector_result) == Float64)
+    @test(eltype(matrix_result) == Int64)
+    @test(eltype(scalar_result) == Float64)
+
+end
+
+# Clean up after ourselves.
+rm(file_name)
